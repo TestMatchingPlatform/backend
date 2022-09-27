470b37471eb304b2ede6403dcc7a9634ae5f6d0f
heeyoung <20171693@edu.hanbat.ac.kr>
2022-09-27 20:44:36 +0900

    <refactor> : <domain수정, jwt추가>
    1. jwt기능만을 모아둔 폴더를 만들었다.
    2. jwt filter와 resolver를 만들었다.
    3. 바뀐 database에 맞춰 domain을 수정했다.
    
    close - #50

--- src/main/java/io/wisoft/testermatchingplatform/config/WebMvcConfig.java
+++ src/main/java/io/wisoft/testermatchingplatform/config/WebMvcConfig.java
@@ -1,17 +1,21 @@
 package io.wisoft.testermatchingplatform.config;
 
+import io.wisoft.testermatchingplatform.jwt.JwtAuthResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @EnableWebMvc
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    private  final JwtAuthResolver authAccountResolver;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -22,4 +26,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .maxAge(3_600);
     }
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(authAccountResolver);
+    }
 }
