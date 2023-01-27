a862be09791d78bdc27bec406bbd337992aaa642
Kukjun Lee <lxx3380@gmail.com>
2023-01-27 22:56:48 +0900

    fix : 배포 시 설정파일 부재, 스크립트 오작동 해결
    배포 할 때, 설정파일을 연동하지 못하는 문제 해결
    * 배포환경에 프로필 등록과 스크립트에 명시적 프로필 사용을 통해 해결
    
    배포 할 때, 스크립트 오작동 해결
    * appspec.yml 에서, 권한 설정 및 스크립트 위치 수정으로 해결
    
    Resolves: #issueNo
    See also: #issueNo

---
+++ scripts/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PROJECT_ROOT=/home/ec2-user/auto_deploy/app
+BUILD_ROOT=/home/ec2-user/auto_deploy/build
+BUILD_JAR_FILE=$BUILD_ROOT/spring-webapp.jar
+
+APP_LOG="$BUILD_JAR_FILE/application.log"
+ERROR_LOG="$BUILD_JAR_FILE/error.log"
+DEPLOY_LOG="#$BUILD_JAR_FILE/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+# build 파일 복사
+echo "$BUILD_JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $BUILD_JAR_FILE
+
+# jar file 실행
+echo "> 새 애플리케이션 배포"
+echo "$TIME_NOW > $BUILD_JAR_FILE 파일 실행" >> $DEPLOY_LOG
+nohup java -jar -Dspring.config.location=classpath:/application.yml,$BUILD_ROOT/application-prod.yml $BUILD_JAR_NAME > $APP_LOG 2> &ERROR_LOG &
+
+# 실행된 프로세스 확인
+CURRENT_PID=$(pgrep -f $BUILD_JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
